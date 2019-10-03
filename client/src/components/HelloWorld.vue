67f875c848bc800152536e4b6dd4df1d7d30a779
Tony Berry <tony@inspiruscu.org>
2019-10-03 16:34:48 -0700

    added response to email request

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -3,14 +3,7 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li>
         <a
           href="https://forum.vuejs.org"
--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cor')
+const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-        message: 'hello world!'
+        message: 'Hello ${req.body.email}! Your user was registered! Have fun!'
     })
 })
-
 app.listen(process.env.PORT || 8081)
-
 
 
